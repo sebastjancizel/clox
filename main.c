fae32bb707a2a6495268792d9eef1de99e3bdbcd
Sebastjan <sebastjancizel@gmail.com>
2023-02-11 21:55:25 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "common.h"
+#include "chunk.h"
+
+int main (int argc, char *argv[])
+{
+	Chunk chunk;
+	initChunk(&chunk);
+	writeChunk(&chunk, OP_RETURN);
+	freeChunk(&chunk);
+	return 0;
+}
