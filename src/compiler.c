d9942c853a85e594a8ec3faab5fd38a85301d050
Sebastjan <sebastjancizel@gmail.com>
2023-03-17 21:54:37 +0000

    wip (293)

--- src/compiler.c
+++ src/compiler.c
@@ -7,4 +7,25 @@
 void compile(const char* source)
 {
     initScanner(source);
+
+    // temporary code to drive the scanner until we actually have a working
+    // compiler
+    int line = -1;
+
+    for (;;)
+    {
+        Token token = scanToken() if (token.line != line)
+        {
+            printf("%4d ", token.line);
+            line = token.line;
+        }
+        else
+        {
+            printf("   | ");
+        }
+    }
+
+    printf(
+        "%2d '%.*s'\n", token.type, token.length,
+        token.start); // *s means print the first n characters of the string
 }
