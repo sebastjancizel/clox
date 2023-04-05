5679762aa1e21cd03afc787045bbd694cdc2b980
Sebastjan <sebastjancizel@gmail.com>
2023-04-05 21:29:36 +0100

    Started on compiling expressions

--- src/compiler.c
+++ src/compiler.c
@@ -1,27 +1,73 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "common.h"
 #include "compiler.h"
 #include "scanner.h"
 
-void compile(const char *source) {
+typedef struct {
+  Token current;
+  Token previous;
+  bool hadError;
+  bool panicMode;
+} Parser;
+
+Parser parser;
+
+static void errorAt(Token *token, const char *message) {
+  if (parser.panicMode)
+    return;
+  parser.panicMode = true;
+
+  fprintf(stderr, "[line %d] Error", token->line);
+
+  if (token->type == TOKEN_EOF) {
+    fprintf(stderr, " at end");
+  } else if (token->type == TOKEN_ERROR) {
+    // Nothing
+  } else {
+    fprintf(stderr, " at '%.*s'", token->length, token->start);
+    // %.*s means print a string of length token->length, starting at
+    // token->start
+  }
+
+  fprintf(stderr, ": %s\n", message);
+  parser.hadError = true;
+}
+
+static void error(const char *message) { errorAt(&parser.previous, message); }
+
+static void errorAtCurrent(const char *message) {
+  errorAt(&parser.current, message);
+}
+
+static void advance(void) {
+  parser.previous = parser.current;
+  for (;;) {
+    parser.current = scanToken();
+    if (parser.current.type != TOKEN_ERROR)
+      break;
+    errorAtCurrent(parser.current.start);
+  }
+}
+
+static void consume(TokenType type, const char *message) {
+  if (parser.current.type == type) {
+    advance();
+    return;
+  }
+
+  errorAtCurrent(message);
+}
+
+bool compile(const char *source, Chunk *chunk) {
   initScanner(source);
 
-  // temporary code to drive the scanner until we actually have a working
-  // compiler
-  int line = -1;
+  parser.hadError = false;
+  parser.panicMode = false;
 
-  for (;;) {
-    Token token = scanToken();
-    if (token.line != line) {
-      printf("%4d ", token.line);
-      line = token.line;
-    } else {
-      printf("   | ");
-    }
-    printf("%2d '%.*s'\n", token.type, token.length,
-           token.start); // *s means print the first n characters of the string
-    if (token.type == TOKEN_EOF)
-      break;
-  }
+  advance();
+  expression();
+  consume(TOKEN_EOF, "Expect end of expression.");
+  return !parser.hadError;
 }
